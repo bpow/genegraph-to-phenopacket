d3f7cbe4a980e057021fd72cec0ce2e1754cc01d
Bradford Powell <bpow@drpowell.org>
2026-04-14 14:51:25 -0400

    fix typos that prevent get-data.bash from running (even though we are moving away from genegraph as source)

--- get-data.bash
+++ get-data.bash
@@ -1,4 +1,4 @@
 DATE=$(date +'%Y%m%d')
-mkdir input/gene-validity-$DATE
+mkdir -p data/input/gene-validity-$DATE
 curl https://storage.googleapis.com/genegraph-public/gene-validity-jsonld-latest.tar.gz \
-	| tar -C input/gene-validity-$DATE +'%Y%m%d') -xvf -
+	| tar -C data/input/gene-validity-$DATE -xzvf -
